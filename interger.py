fea158d9a062f468c1fef12d538f8c11a437cf82
Ubuntu <ubuntu@ip-172-31-74-87.ec2.internal>
2022-04-03 01:52:44 +0000

    added interger.py

---
+++ interger.py
@@ -0,0 +1,19 @@
+# this program determines if a an inter is
+# postitive, negative or 0
+
+# input
+guess = int(input("enter an integer"))
+print("")
+
+# process
+n = 0
+
+# output
+
+print("")
+if guess > n:
+    print("{} is positive number".format(guess))
+elif guess < n:
+    print("{} is negative number".format(guess))
+else:
+    print("{} is just zero ".format(guess))
